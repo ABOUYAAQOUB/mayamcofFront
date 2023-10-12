2188e5fc6cabd54f90fe9aec7c3d8be076533670
yassine EL Bouazzaoui <67290757+Yassine-ELBouazzaoui@users.noreply.github.com>
2023-10-12 15:55:47 +0100

    modification sur consulte

--- src/app/application/admin/facture/consult/consult.component.ts
+++ src/app/application/admin/facture/consult/consult.component.ts
@@ -1,8 +1,8 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { Observable } from 'rxjs';
 import { Construction } from 'src/app/_core/models/construction';
 import { Facture } from 'src/app/_core/models/facture';
 import { ConstructionService } from 'src/app/_core/service/construction.service';
@@ -12,13 +12,23 @@ import { SuppressionComponent } from 'src/app/_shear/dialog/suppression/suppress
 @Component({
   selector: 'app-consult',
   templateUrl: './consult.component.html',
-  styleUrls: ['./consult.component.css']
+  styleUrls: ['./consult.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ConsultComponent implements OnInit {
   
-  displayedColumns: string[] = ['id', 'datefacture','terrain','client','action'];
+  columnsToDisplay: string[] = ['id', 'datefacture','terrain','client'];
   constructions:Construction[]|any = null;
   dataSource = new MatTableDataSource<Facture>();
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement: Facture | any;
+  total:number=0;
   constructor(private factureService: FactureService,private construction:ConstructionService,public dialog: MatDialog) { }
   
   ngOnInit(): void {
@@ -85,6 +95,13 @@ export class ConsultComponent implements OnInit {
       }
     });
    return nom;
+  }
+
+  getTotale(id:number):Number{
+    this.constructions.filter((cons:Construction)=>cons.facture!==null && cons.facture.id==id).forEach((element:Construction)=>{
+      this.total+=Number(element.prix)*Number(element.quantite);
+    })
+    return this.total;
   }
 
   
